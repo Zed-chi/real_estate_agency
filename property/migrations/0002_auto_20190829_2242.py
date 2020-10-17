3dfc86202e564424bd8ebf4758103fb89dbd42f6
zed <zed@mail.ru>
2020-10-17 21:55:30 +0300

    added search to flasts

--- property/migrations/0002_auto_20190829_2242.py
+++ property/migrations/0002_auto_20190829_2242.py
@@ -6,18 +6,28 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('property', '0001_initial'),
+        ("property", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='flat',
-            name='construction_year',
-            field=models.IntegerField(blank=True, db_index=True, null=True, verbose_name='Год постройки здания'),
+            model_name="flat",
+            name="construction_year",
+            field=models.IntegerField(
+                blank=True,
+                db_index=True,
+                null=True,
+                verbose_name="Год постройки здания",
+            ),
         ),
         migrations.AlterField(
-            model_name='flat',
-            name='living_area',
-            field=models.IntegerField(blank=True, db_index=True, null=True, verbose_name='количество жилых кв.метров'),
+            model_name="flat",
+            name="living_area",
+            field=models.IntegerField(
+                blank=True,
+                db_index=True,
+                null=True,
+                verbose_name="количество жилых кв.метров",
+            ),
         ),
     ]
