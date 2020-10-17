0b919866697019fe075d34824685ab1e5b14ae08
zed <zed@mail.ru>
2020-10-17 22:58:38 +0300

    added editable field to flat model

--- property/migrations/0003_flat_new_building.py
+++ property/migrations/0003_flat_new_building.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('property', '0002_auto_20190829_2242'),
+        ("property", "0002_auto_20190829_2242"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='flat',
-            name='new_building',
+            model_name="flat",
+            name="new_building",
             field=models.NullBooleanField(),
         ),
     ]
