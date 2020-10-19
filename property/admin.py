6ff0a38a78e799b07f454557cb4cd0900547f141
zed-chi <zed-chi@mail.ru>
2020-10-19 15:55:25 +0300

    added list filters to flat admin model

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,18 @@ class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ["created_at"]
     list_display = ("address", "price", "new_building", "created_at", "town")
     list_editable = ["new_building"]
+    list_filter = [
+        "new_building",
+        "owners_phonenumber",
+        "created_at",
+        "price",
+        "town",
+        "floor",
+        "rooms_number",
+        "living_area",
+        "has_balcony",
+        "construction_year",
+    ]
 
 
 admin.site.register(Flat, FlatAdmin)
